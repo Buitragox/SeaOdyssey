535cd35174926ff64bc2a92b1594ba6078d0888f
Buitragox <jhoanuitrago@gmail.com>
2023-05-19 17:34:33 -0500

    Add bullet collisions

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -3,20 +3,32 @@ extends KinematicBody2D
 class_name Bullet
 
 var speed = 600
-
+var damage = 25 #Default value
 var velocity = Vector2.ZERO
+
+func set_bullet_data(pos, vel, color):
+	$Polygon2D.color = color
+	global_position = pos
+	velocity = vel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	#position = Vector2(2000, 2000)
 	
 func _init():
 	#Spawn outside of screen so it doesn't collide with the ship
 	position = Vector2(2000, 2000)
+
+func _process(delta):
+	if not $VisibilityNotifier2D.is_on_screen():
+		queue_free()
 
 func _physics_process(delta):
 	
 	velocity = velocity.normalized() * speed * delta;
 	
 	var collision = move_and_collide(velocity)
+	if collision:
+		if collision.has_method("hit"):
+			collision.hit(damage)
+		queue_free()
--- scripts/config.gd
+++ scripts/config.gd
@@ -1,2 +1,9 @@
 const WIDTH = 1280
 const HEIGHT = 720
+
+enum Layers {
+	SHIP,
+	ENEMY,
+	ALLY_BULLET,
+	ENEMY_BULLET	
+}
